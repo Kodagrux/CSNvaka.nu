b063c48bc10d9f20f7c81ced20a9f0fe6cc0e2a6
Andreas Grünewald <a.grunewald2@gmail.com>
2015-10-10 19:21:08 +0200

    graph working
    Started styling n stuff

--- andreas/js/chartScript.js
+++ andreas/js/chartScript.js
@@ -1,27 +1,40 @@
-Chart.defaults.global = {
-	animation: true, 
-	
-}
 
 $(document).ready(function () 
-{
+{	
 	var data = {
 		labels: ["23.00", "23.05", "23.10", "23.15", "23.20", "23.25", "23.30", "23.35", "23.40", "23.45", "23.50", "23.55", "00.00"],
+		// labels: ["", "", "", "", "", "", "", "", "", "", "", "", ""],
 		datasets: [
 			{
 				label: "Time",
-				fillColor: "rgba(220,220,220,0.5)",
-				strokeColor: "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(220,220,220,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
+				fillColor: "rgba(220,220,220,0.2)",
+				strokeColor: "rgba(255,255,255,0)",
+				highlightFill: "rgba(220,220,220,0.2)",
+				highlightStroke: "rgba(220,220,220,.4)",
 				data: [65, 59, 80, 81, 56, 55, 40, 45, 50, 55, 65, 78, 23]
 			}
 		]
 	};
 
 	var ctx = document.getElementById("myChart").getContext("2d");
-	new Chart(ctx).Bar(data, {
-	    // barShowStroke: false
+	
+	var myNewChart = new Chart(ctx).Bar(data, {
+	    barShowStroke: true,
+	    responsive: true,
+	    
+	    scaleShowLabels : false,
+	    scaleShowGridLines: false,
+	    barStrokeWidth: 1,
+	    barValueSpacing: 12,
+	    scaleLineColor: "transparent",
+	    scaleFontColor: "rgba(255,255,255,0",
+
+	    customToolTips: true,
+	    tooltipEvents: ["mousemove", "touchstart", "touchmove"],
+	    tooltipFillColor: "rgba(0,0,0,0.8)",
+	    tooltipTemplate: "<%= value %>",
+	    scaleLabel: "<%=value%>",
+
 	});
 });
 
